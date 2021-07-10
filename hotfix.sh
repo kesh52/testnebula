71be7793e3c5724c1ec0e59f975ac99d62b96e3d
Ilja Michajlow <ilja.michajlow@gmail.com>
2021-07-10 13:44:06 +0200

    test

--- hotfix.sh
+++ hotfix.sh
@@ -1,4 +1,5 @@
 echo "x" >> HELP.md
 git add --all
 git commit -a -m "test"
-./gradlew -Prelease.sanitizeVersion=true devSnapshot -Prelease.scope=patch
+# ./gradlew -Prelease.sanitizeVersion=true devSnapshot -Prelease.scope=patch
+./gradlew -Prelease.sanitizeVersion=true snapshot -Prelease.scope=patch
