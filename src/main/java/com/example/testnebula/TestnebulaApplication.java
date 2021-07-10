f5200f960943d6725fe52e0fe8abe006406be413
Ilja Michajlow <ilja.michajlow@gmail.com>
2021-07-10 13:01:14 +0200

    init

---
+++ src/main/java/com/example/testnebula/TestnebulaApplication.java
@@ -0,0 +1,13 @@
+package com.example.testnebula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestnebulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestnebulaApplication.class, args);
+	}
+
+}
